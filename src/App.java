aa13bf8487503e8cb7c732c39b8a95b8976a1da6
Nicolas Docolas <nicolas.docolas1804@gmail.com>
2025-03-10 21:31:32 -0300

    SEGUNDO commit

--- src/App.java
+++ src/App.java
@@ -3,7 +3,8 @@ public class App {
 
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
         Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
-
+        Carro utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL, 5, 70);
+        
         System.out.println("Tipos de veiculos:");
         System.out.println(basico);
 
